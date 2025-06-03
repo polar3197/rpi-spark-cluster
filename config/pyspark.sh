a94b31b1a86e16e0b414a4a9c9e317f8a499583a
Your Name <charlie.c@berkeley.edu>
2025-06-03 11:25:37 -0700

    a

---
+++ config/pyspark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pyspark \
+  --master spark://192.168.0.32:7077 \
+  --conf spark.driver.host=192.168.0.32 \
+  --conf spark.driver.bindAddress=0.0.0.0 \
+  --conf spark.executor.memory=512m \
+  --conf spark.driver.memory=512m
--- master/start-master.sh
+++ master/start-master.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export PYSPARK_SUBMIT_ARGS="--master spark://192.168.1.116:7077 pyspark-shell"
+
 echo "Starting Spark master in foreground"
 
 exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master \
