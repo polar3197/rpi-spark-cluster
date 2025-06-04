4f4b4da47106b91425290048a61ca4d42e5691ce
Your Name <charlie.c@berkeley.edu>
2025-06-04 16:49:58 -0700

    resolve DNS

--- config/pyspark-m.sh
+++ config/pyspark-m.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pyspark \
-  --master spark://192.168.0.32:7077 \
+  --master spark://spark-master:7077 \
   --conf spark.driver.host=spark-master \
   --conf spark.driver.bindAddress=0.0.0.0 \
   --conf spark.executor.memory=512m \
