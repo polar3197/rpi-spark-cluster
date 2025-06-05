86584d5c72d3bab2d3e591f639f00d3958d7e193
Your Name <charlie.c@berkeley.edu>
2025-06-04 19:01:00 -0700

    trying to grasp it all

--- master/start-master.sh
+++ master/start-master.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-export PYSPARK_SUBMIT_ARGS="--master spark://192.168.1.116:7077 pyspark-shell"
+echo "Starting Spark master at ${SPARK_MASTER_HOST:-localhost}:7077"
 
-echo "Starting Spark master in foreground"
-
-exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master \
-  --host 0.0.0.0 \
+exec ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master \
+  --host "${SPARK_MASTER_HOST:-localhost}" \
   --port 7077 \
   --webui-port 8080
 
--- worker/start-worker.sh
+++ worker/start-worker.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-: "${SPARK_MASTER_URL:=spark://spark-master:7077}"
+echo "Starting Spark worker with ${SPARK_WORKER_CORES:-2} cores and ${SPARK_WORKER_MEMORY:-2g}, connecting to ${SPARK_MASTER_URL}"
 
-echo "Starting Spark worker, connecting to $SPARK_MASTER_URL"
-
-exec $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker \
-  --cores ${SPARK_WORKER_CORES:-2} \
-  --memory ${SPARK_WORKER_MEMORY:-1g} \
-  $SPARK_MASTER_URL
+exec ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker \
+  --cores "${SPARK_WORKER_CORES:-2}" \
+  --memory "${SPARK_WORKER_MEMORY:-2g}" \
+  "${SPARK_MASTER_URL}"
 
