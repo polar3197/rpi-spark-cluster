f9509c03c29b1ad658368f1757a9e0e5f85174af
Your Name <charlie.c@berkeley.edu>
2025-06-11 15:16:07 -0700

    hsiouv

--- config/pyspark-m.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pyspark \
-  --master spark://spark-master:7077 \
-  --conf spark.driver.host=spark-master \
-  --conf spark.driver.bindAddress=0.0.0.0 \
-  --conf spark.executor.memory=512m \
-  --conf spark.driver.memory=512m
