01bf68c32e110d1192055b82cb32afbd6de1467e
Your Name <charlie.c@berkeley.edu>
2025-06-10 17:27:55 -0700

    ijwoif

--- worker/start-worker.sh
+++ worker/start-worker.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
-export SPARK_LOCAL_IP=$(ip route get 192.168.0.32 | awk '/src/ { print $7; exit }')
+
+# Dynamically determine the worker's LAN IP
+export SPARK_LOCAL_IP=$(ip route get ${SPARK_MASTER_HOST:-192.168.0.32} | awk '/src/ { print $7; exit }')
+export SPARK_PUBLIC_DNS=$SPARK_LOCAL_IP
 
 echo "Detected local IP: $SPARK_LOCAL_IP"
-echo "Starting Spark worker with SPARK_LOCAL_IP=$SPARK_LOCAL_IP \n with ${SPARK_WORKER_CORES:-2} cores and ${SPARK_WORKER_MEMORY:-2g} \n connecting to ${SPARK_MASTER_URL}"
+echo "Starting Spark worker with SPARK_LOCAL_IP=$SPARK_LOCAL_IP \n with ${SPARK_WORKER_CORES:-2} cores and ${SPARK_WORKER_MEMORY:-2g}" 
+echo "Connecting to ${SPARK_MASTER_URL}"
 
 exec ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker \
   --cores "${SPARK_WORKER_CORES:-2}" \
