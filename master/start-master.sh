8742b3e3444f794346eeb62fd6fbaea0a83833a9
Your Name <charlie.c@berkeley.edu>
2025-06-14 15:19:24 -0700

    hiu

--- master/start-master.sh
+++ master/start-master.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Starting Spark master at ${SPARK_MASTER_HOST_IP:-localhost}:7077"
+echo "Starting Spark master at ${SPARK_MASTER_HOST_IP:-0.0.0.0}:7077"
 
 exec ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master \
-  --host "${SPARK_MASTER_HOST:-localhost}" \
+  --host 0.0.0.0 \
   --port 7077 \
   --webui-port 8080
 
