f19a0f739d2f5fba25b257b2cdfad7e24d496a07
Your Name <charlie.c@berkeley.edu>
2025-06-14 15:23:41 -0700

    svs

--- worker/start-worker.sh
+++ worker/start-worker.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+# Source environment
+. "${SPARK_HOME}/conf/spark-env.sh"
+
 echo "Starting Spark worker with SPARK_LOCAL_IP=${SPARK_LOCAL_IP:-0.0.0.0}"
+echo "Connecting to ${SPARK_MASTER_URL}"
 
 exec ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.worker.Worker \
   --host "${SPARK_LOCAL_IP:-0.0.0.0}" \
