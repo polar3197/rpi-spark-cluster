0f535612af4a6b5a5ad63c70bc1f9b5f1addfd0b
Your Name <charlie.c@berkeley.edu>
2025-06-23 19:29:21 -0700

    autopilot

--- worker/start-worker.sh
+++ worker/start-worker.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Source environment variables
-. "${SPARK_HOME}/conf/spark-env.sh"
-
 echo "Starting Spark worker with SPARK_LOCAL_IP=$SPARK_LOCAL_IP"
 echo "Connecting to ${SPARK_MASTER_URL}"
 
