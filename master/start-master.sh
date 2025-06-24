cb1337724a4444bdf56d383fb3a6a5f16807f826
Your Name <charlie.c@berkeley.edu>
2025-06-23 17:45:36 -0700

    nuive

--- master/start-master.sh
+++ master/start-master.sh
@@ -4,6 +4,7 @@ set -e
 echo "Starting Spark master at 192.168.0.32:7077"
 
 exec ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master \
+  --host 192.168.0.32 \
   --port 7077 \
   --webui-port 8080
 
