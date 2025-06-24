28ae05436fee8531c1837b09aaa48697c4693cd8
Your Name <charlie.c@berkeley.edu>
2025-06-23 18:48:54 -0700

    autopilot

--- master/start-master.sh
+++ master/start-master.sh
@@ -3,10 +3,8 @@ set -e
 
 echo "Starting Spark master"
 
-# Source and export environment variables from spark-env.sh
-set -a  # Automatically export all variables
+# Source environment variables
 . "${SPARK_HOME}/conf/spark-env.sh"
-set +a
 
 exec ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master \
   --host 0.0.0.0 \
--- worker/start-worker.sh
+++ worker/start-worker.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-# Source and export environment variables from spark-env.sh
-set -a  # Automatically export all variables
+# Source environment variables
 . "${SPARK_HOME}/conf/spark-env.sh"
-set +a
 
 echo "Starting Spark worker with SPARK_LOCAL_IP=$SPARK_LOCAL_IP"
 echo "Connecting to ${SPARK_MASTER_URL}"
