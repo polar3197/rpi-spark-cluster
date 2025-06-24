5a65c3aae2c9dd97e970106b448f818deec71945
Your Name <charlie.c@berkeley.edu>
2025-06-23 18:30:58 -0700

    autopilot

--- master/start-master.sh
+++ master/start-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Starting Spark master at 192.168.0.32:7077"
+echo "Starting Spark master"
 
 if [ -f "${SPARK_HOME}/conf/spark-env.sh" ]; then
   . "${SPARK_HOME}/conf/spark-env.sh"
@@ -9,7 +9,7 @@ fi
 
 exec ${SPARK_HOME}/bin/spark-class org.apache.spark.deploy.master.Master \
   --host 0.0.0.0 \
-  --ip "$SPARK_PUBLIC_DNS" \
+  # --ip "$SPARK_PUBLIC_DNS" \
   --port 7077 \
   --webui-port 8080
 
