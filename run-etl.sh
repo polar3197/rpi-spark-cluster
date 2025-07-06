aeba79199551737ea2b1688cf7e6eb582017de91
Your Name <charlie.c@berkeley.edu>
2025-07-05 22:55:42 -0700

    dscd

--- run-etl.sh
+++ run-etl.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /Users/charliecooper/Code/projects/rpi-cluster/.picl-venv/bin/python \
-  /Users/charliecooper/Code/projects/rpi-cluster/BART/bart_etl.py
+  /Users/charliecooper/Code/projects/rpi-cluster/BART/embr-etl.py
---
+++ run-rock-etl.sh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/home/charlie.c/rpi-spark-cluster/.picl-venv/bin/python \
+  /home/charlie.c/rpi-spark-cluster/BART/rock-etl.py
